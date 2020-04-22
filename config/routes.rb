28da76cc73d75acf637a98354c077b081388dfde
ebrohman <ericbrohman@gmail.com>
2020-04-21 18:40:22 -0600

    Add some basic routes
    For creating messages, getting a list of users,
    and showing a specific user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :messages, only: [:create]
+
+  resources :users, only: [:index, :show] do
+    get :messages
+  end
 end
