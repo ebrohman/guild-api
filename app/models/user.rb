0d1cc493efadf630e0bd7c22127c77f94cfe1fed
ebrohman <ericbrohman@gmail.com>
2020-04-24 16:45:24 -0600

    Remove unused association methods from User
    These methods were declared as I began to
    set up the app, but I didn't end up using them,
    so there's no point in keeping them around.
    
    Removing the test cases for these associations as well.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :outbound_messages, class_name: "Message", foreign_key: :sender_id
-  has_many :inbound_messages, class_name: "Message", foreign_key: :recipient_id
 end
